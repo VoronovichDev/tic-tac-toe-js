db038dc30851d006bd63963ef8424bf89a653880
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-23 22:56:16 +0300

    created playerFactory function and Gamebord module with renderGamboard method

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const playerFactory = (mark) => {
+   return { mark }
+}
+
+const player1 = playerFactory('X')
+const player2 = playerFactory('O')
+
+const Gameboard = (() => {
+
+   const gameField = document.querySelector('#game')
+
+   let fieldContent = ['X1', 'O2', 'X3', 'O4', 'X5', 'O6', 'X7', 'O8', 'X9']
+
+   const renderGameboard = () => {
+      fieldContent.forEach((el) => {
+         const cell = document.createElement('div')
+         cell.innerText = el;
+         gameField.append(cell)
+      })
+   }
+   renderGameboard()
+})()
+
