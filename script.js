aa5ce7adc2d0338a9e68f6ccfd80caf0a65a8359
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-24 23:47:13 +0300

    finished js-logic, connected font

--- script.js
+++ script.js
@@ -12,12 +12,14 @@ const Gameboard = (() => {
    let fieldContent = ['', '', '', '', '', '', '', '', '']
 
    const renderGameboard = () => {
+
       // addin ID for each cell
       let cellId = 0;
 
       // fill our field with empty cells
       fieldContent.forEach((el) => {
          const cell = document.createElement('div')
+
          //give id for each cell
          cell.setAttribute("cellid", cellId)
          cell.innerText = el;
@@ -25,12 +27,12 @@ const Gameboard = (() => {
          cellId++
       })
 
-      gameField.addEventListener('click', (e) => {
 
+      gameField.addEventListener('click', cellClicked = (e) => {
          if (!e.target.getAttribute('cellid')) return
 
-         let xLength = fieldContent.filter(elem => elem == "X").length
-         let oLength = fieldContent.filter(elem => elem == "O").length
+         let xLength = fieldContent.filter(elem => elem === "X").length
+         let oLength = fieldContent.filter(elem => elem === "O").length
 
          // checks the content and inserts "X" or "O"
          if (xLength === 0 || xLength !== 0 && xLength === oLength) {
@@ -41,77 +43,129 @@ const Gameboard = (() => {
             e.target.innerText = player2.mark
          }
 
-         console.log(e.target.getAttribute('cellid'))
+         // layout for "X" wins
+         let winX = document.createElement('h2')
+         winX.classList.add('result')
+         winX.innerText = 'Player 1 wins'
+
+         // layout for "O" wins
+         let winO = document.createElement('h2')
+         winO.classList.add('result')
+         winO.innerText = 'Player 2 wins'
+
+         // layout for draw
+         let draw = document.createElement('h2')
+         draw.classList.add('result')
+         draw.innerText = 'Draw'
+
+         // array from each cell of gameboard. Used for decoration in win cases
+         let cellArray = Array.from(gameField.children)
+
+         //logic of filling out the document, in case of victory X
+         let winXContent = () => {
+            document.body.append(winX)
+            document.body.append(resetBtn)
+            gameField.removeEventListener('click', cellClicked)
+         }
+
+         //logic of filling out the document, in case of victory O
+         let winOContent = () => {
+            document.body.append(winO)
+            document.body.append(resetBtn)
+            gameField.removeEventListener('click', cellClicked)
+         }
 
          // fill our array (fieldContent) with marks
          fieldContent[e.target.getAttribute('cellid')] = e.target.innerText
-         console.log(fieldContent)
 
-
-         if (fieldContent[0] == fieldContent[1] && fieldContent[1] == fieldContent[2] && fieldContent[2] == 'X') {
-            document.body.append(winX)
-            document.body.append(resetBtn)
+         //victory conditions
+         if (fieldContent[0] === fieldContent[1] && fieldContent[1] === fieldContent[2] && fieldContent[2] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 0 || +e.getAttribute('cellid') === 1 || +e.getAttribute('cellid') === 2).forEach(e => e.style.backgroundColor = '#34eb67')
             return
-         } else if (fieldContent[0] == fieldContent[1] && fieldContent[1] == fieldContent[2] && fieldContent[2] == 'O') {
-            document.body.append(winO)
-            document.body.append(resetBtn)
+         } else if (fieldContent[0] === fieldContent[1] && fieldContent[1] === fieldContent[2] && fieldContent[2] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 0 || +e.getAttribute('cellid') === 1 || +e.getAttribute('cellid') === 2).forEach(e => e.style.backgroundColor = '#34eb67')
             return
+         } else if (fieldContent[3] === fieldContent[4] && fieldContent[4] === fieldContent[5] && fieldContent[5] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 3 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 5).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[3] === fieldContent[4] && fieldContent[4] === fieldContent[5] && fieldContent[5] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 3 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 5).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[6] === fieldContent[7] && fieldContent[7] === fieldContent[8] && fieldContent[8] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 6 || +e.getAttribute('cellid') === 7 || +e.getAttribute('cellid') === 8).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[6] === fieldContent[7] && fieldContent[7] === fieldContent[8] && fieldContent[8] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 6 || +e.getAttribute('cellid') === 7 || +e.getAttribute('cellid') === 8).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[0] === fieldContent[4] && fieldContent[4] === fieldContent[8] && fieldContent[8] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 0 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 8).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[0] === fieldContent[4] && fieldContent[4] === fieldContent[8] && fieldContent[8] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 0 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 8).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[2] === fieldContent[4] && fieldContent[4] === fieldContent[6] && fieldContent[6] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 2 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 6).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[2] === fieldContent[4] && fieldContent[4] === fieldContent[6] && fieldContent[6] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 2 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 6).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[0] === fieldContent[3] && fieldContent[3] === fieldContent[6] && fieldContent[6] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 0 || +e.getAttribute('cellid') === 3 || +e.getAttribute('cellid') === 6).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[0] === fieldContent[3] && fieldContent[3] === fieldContent[6] && fieldContent[6] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 0 || +e.getAttribute('cellid') === 3 || +e.getAttribute('cellid') === 6).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[1] === fieldContent[4] && fieldContent[4] === fieldContent[7] && fieldContent[7] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 1 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 7).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[1] === fieldContent[4] && fieldContent[4] === fieldContent[7] && fieldContent[7] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 1 || +e.getAttribute('cellid') === 4 || +e.getAttribute('cellid') === 7).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[2] === fieldContent[5] && fieldContent[5] === fieldContent[8] && fieldContent[8] === 'X') {
+            winXContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 2 || +e.getAttribute('cellid') === 5 || +e.getAttribute('cellid') === 8).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } else if (fieldContent[2] === fieldContent[5] && fieldContent[5] === fieldContent[8] && fieldContent[8] === 'O') {
+            winOContent()
+            cellArray.filter(e => +e.getAttribute('cellid') === 2 || +e.getAttribute('cellid') === 5 || +e.getAttribute('cellid') === 8).forEach(e => e.style.backgroundColor = '#34eb67')
+            return
+         } //draw conditions 
+         else if (xLength === oLength && oLength === 4) {
+            document.body.append(draw)
+            document.body.append(resetBtn)
+            gameField.removeEventListener('click', cellClicked)
+            cellArray.forEach(e => e.style.backgroundColor = 'rgb(254, 238, 91)')
          }
-
-         if (fieldContent[3] == fieldContent[4] && fieldContent[4] == fieldContent[5] && fieldContent[5] == 'X') {
-            document.body.append(winX)
-            document.body.append(resetBtn)
-            return
-         } else if (fieldContent[3] == fieldContent[4] && fieldContent[4] == fieldContent[5] && fieldContent[5] == 'O') {
-            document.body.append(winO)
-            document.body.append(resetBtn)
-            return
-         }
-
-         if (fieldContent[6] == fieldContent[7] && fieldContent[7] == fieldContent[8] && fieldContent[8] == 'X') {
-            document.body.append(winX)
-            document.body.append(resetBtn)
-            return
-         } else if (fieldContent[6] == fieldContent[7] && fieldContent[7] == fieldContent[8] && fieldContent[8] == 'O') {
-            document.body.append(winO)
-            document.body.append(resetBtn)
-            return
-         }
-
-         if (fieldContent[0] == fieldContent[4] && fieldContent[4] == fieldContent[8] && fieldContent[8] == 'X') {
-            document.body.append(winX)
-            document.body.append(resetBtn)
-            return
-         } else if (fieldContent[0] == fieldContent[4] && fieldContent[4] == fieldContent[8] && fieldContent[8] == 'O') {
-            document.body.append(winO)
-            document.body.append(resetBtn)
-            return
-         }
-
-         if (fieldContent[2] == fieldContent[4] && fieldContent[4] == fieldContent[6] && fieldContent[6] == 'X') {
-            document.body.append(winX)
-            document.body.append(resetBtn)
-            return
-         } else if (fieldContent[2] == fieldContent[4] && fieldContent[4] == fieldContent[6] && fieldContent[6] == 'O') {
-            document.body.append(winO)
-            document.body.append(resetBtn)
-            return
-         }
-
       })
+
+      //restart game
+      resetBtn.onclick = () => {
+         fieldContent = ['', '', '', '', '', '', '', '', ''];
+         gameField.innerHTML = ''
+         renderGameboard()
+         document.body.querySelector('.result').remove()
+         resetBtn.remove()
+      }
    }
 
-   let winX = document.createElement('h2')
-   winX.classList.add('result')
-   winX.innerText = 'Player 1 wins'
-
-   let winO = document.createElement('h2')
-   winO.classList.add('result')
-   winO.innerText = 'Player 2 wins'
-
+   // button for restart
    let resetBtn = document.createElement('button')
+   resetBtn.classList.add('reset__btn')
    resetBtn.innerText = 'Start new game'
-
 
    renderGameboard()
 
