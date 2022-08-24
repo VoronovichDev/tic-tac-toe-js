2053839ca03566d21a4b16aa01b9fe20a2d9c848
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-24 11:40:44 +0300

    added take turns logic, fill fieldContetnt-array with marks

--- script.js
+++ script.js
@@ -9,15 +9,46 @@ const Gameboard = (() => {
 
    const gameField = document.querySelector('#game')
 
-   let fieldContent = ['X1', 'O2', 'X3', 'O4', 'X5', 'O6', 'X7', 'O8', 'X9']
+   let fieldContent = ['', '', '', '', '', '', '', '', '']
 
    const renderGameboard = () => {
+      // addin ID for each cell
+      let cellId = 0;
+
+      // fill our field with empty cells
       fieldContent.forEach((el) => {
          const cell = document.createElement('div')
+         //give id for each cell
+         cell.setAttribute("cellid", cellId)
          cell.innerText = el;
          gameField.append(cell)
+         cellId++
+      })
+
+      gameField.addEventListener('click', (e) => {
+
+         let xLength = fieldContent.filter(elem => elem == "X").length
+         let oLength = fieldContent.filter(elem => elem == "O").length
+
+
+         // checks the content and inserts "X" or "O"
+         if (xLength === 0 || xLength !== 0 && xLength === oLength) {
+            if (e.target.innerText != '') return
+            e.target.innerText = player1.mark
+         } else if (xLength > oLength) {
+            if (e.target.innerText != '') return
+            e.target.innerText = player2.mark
+         }
+
+         console.log(e.target.getAttribute('cellid'))
+
+         // fill our array (fieldContent) with marks
+         fieldContent[e.target.getAttribute('cellid')] = e.target.innerText
+         console.log(fieldContent)
+
       })
    }
+
    renderGameboard()
 })()
 
